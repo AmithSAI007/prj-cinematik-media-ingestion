b3d8f06e1210c76e891eb6144ff0ded3498bb200
AmithSAI007 <amith@dataarize.com>
2025-02-09 21:35:37 +0530

    feat: add structured logging

--- pkg/transform/transform.go
+++ pkg/transform/transform.go
@@ -8,7 +8,10 @@ import (
 // It uses the information from the StorageObjectData, such as the bucket, name, and time created,
 // to populate the corresponding fields in the TopicMessgaeData. The ContentType and Size fields are
 // left empty or set to zero as they are not available in the input data.
-func TransformToTopicMessageData(data StorageObjectData) TopicMessgaeData {
+func TransformToTopicMessageData(data StorageObjectData) (TopicMessgaeData, error) {
+	if data.Bucket == "" || data.Name == "" {
+		return TopicMessgaeData{}, fmt.Errorf("invalid input: bucket or name is empty")
+	}
 	return TopicMessgaeData{
 		Bucket:      data.Bucket,
 		FileName:    data.Name,
@@ -16,5 +19,5 @@ func TransformToTopicMessageData(data StorageObjectData) TopicMessgaeData {
 		ContentType: data.ContentType,
 		Size:        data.Size,
 		TimeCreated: data.TimeCreated,
-	}
+	}, nil
 }
